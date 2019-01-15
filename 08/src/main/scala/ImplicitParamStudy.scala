1d0c0f681b14c60363bb8127622dd6dd1afc23c4
suaaa7 <spuaaa7@gmail.com>
2019-01-16 00:46:37 +0900

    update ImplicitParamStudy.scala

--- 08/src/main/scala/ImplicitParamStudy.scala
+++ 08/src/main/scala/ImplicitParamStudy.scala
@@ -4,4 +4,20 @@ object ImplicitParamStudy {
     def executeQuery(query: String): Unit = println(s"Execute: ${query}")
   }
 
+  def createTitle(title: String)(implicit conn: Connection): Unit = {
+    conn.executeQuery(s"create title='${title}'")
+  }
+
+  def selectTitle(implicit conn: Connection): Unit = {
+    conn.executeQuery(s"select")
+  }
+
+  def updateTitle(title: String)(implicit conn: Connection): Unit = {
+    conn.executeQuery(s"update title='${title}'")
+  }
+
+  def deleteTitle(title: String)(implicit conn: Connection): Unit = {
+    conn.executeQuery(s"delete title='${title}'")
+  }
+
 }
