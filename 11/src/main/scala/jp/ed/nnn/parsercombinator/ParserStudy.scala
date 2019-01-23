f5a509719b033cb6cd08fab96db925ea52e24d64
suaaa7 <spuaaa7@gmail.com>
2019-01-23 22:39:13 +0900

    add ParserStudy.scala

---
+++ 11/src/main/scala/jp/ed/nnn/parsercombinator/ParserStudy.scala
@@ -0,0 +1,17 @@
+package jp.ed.nnn.parsercombinator
+
+object ParserStudy {
+  sealed trait ParseResult[+T]
+  case class Success[+T](value: T, next: String) extends ParseResult[T]
+  case object Failure extends ParseResult[Nothing]
+
+  type Parser[+T] = String => ParseResult[T]
+
+  def trueParser: Parser[Boolean] = input =>
+    if (input.startsWith("true")) {
+      Success(true, input.substring("true".length))
+    } else {
+      Failure
+    }
+
+}
